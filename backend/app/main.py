3f07a15bb5b0e48dc127093b3c47087d6282d77d
Bnowako <blazejnowakowski98@gmail.com>
2025-03-18 12:56:05 +0100

    add initial frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,17 @@
 from app.config import create_app
-
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
 app = create_app()
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/status")
 async def main():
     return {"status": "OK"}
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
 
