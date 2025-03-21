f270bc7ca3ed408e11886986cc05960d234c07b7
Bnowako <blazejnowakowski98@gmail.com>
2025-03-21 17:24:51 +0100

    asd

--- bro-use/src/index.ts
+++ bro-use/src/index.ts
@@ -1,14 +1,41 @@
 import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import StagehandConfig from './stagehand.config';
+import { BrowserContext, Page, Stagehand } from '@browserbasehq/stagehand';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello from Express + TypeScript!');
+const stagehand = new Stagehand({
+  ...StagehandConfig,
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+let page: Page;
+let context: BrowserContext;
+
+async function main() {
+  try {
+    await stagehand.init();
+    page = stagehand.page;
+    context = stagehand.context;
+
+    app.get('/', async (req: Request, res: Response) => {
+      await page.goto("https://docs.stagehand.dev");
+      res.send('Hello from Express + TypeScript!');
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+
+  } catch (err) {
+    console.error('Stagehand initialization failed', err);
+    process.exit(1);
+  }
+}
+
+main();
